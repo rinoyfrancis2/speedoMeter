c59b715ff6d9605bdda425cbd733d11f71abb4fc
RinoySayone <rinoy@sayonetech.com>
2020-10-29 11:27:52 +0530

    Initial Commit

---
+++ speedoMeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  speedoMeter
+//
+//  Created by Rinoy Francis on 29/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
